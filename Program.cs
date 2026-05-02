7439e4be015981fca7db2f5a4599cff5eb875ccf
Vinicius Guedes <viniciusguedes.1007@outlook.com>
2026-05-02 15:05:20 -0300

    Liberando acesso publico

--- Program.cs
+++ Program.cs
@@ -2,19 +2,41 @@ using Pizzaria.API.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Controllers
 builder.Services.AddControllers();
+
+// Repositories
 builder.Services.AddScoped<PizzaRepository>();
 builder.Services.AddScoped<UsuarioRepository>();
 builder.Services.AddScoped<VendaRepository>();
+
+// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// 🔥 CORS LIBERADO GERAL
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("LiberadoGeral", policy =>
+    {
+        policy
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
+// Swagger
 app.UseSwagger();
 app.UseSwaggerUI();
 
+// 🔥 AQUI É O PULO DO GATO (antes do MapControllers)
+app.UseCors("LiberadoGeral");
+
 app.UseHttpsRedirection();
+
 app.MapControllers();
 
 app.Run();
